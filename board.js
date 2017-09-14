6fd64a3f7150ee46c4ef9bdd412a218290569496
Blanca Sanchez <blancayllen@gmail.com>
2017-09-14 18:35:10 -0400

    added code to board game

--- board.js
+++ board.js
@@ -1,11 +1,11 @@
 /**
  * Given a width and height, construct a Board.
- * 
- * @param {Int} width 
- * @param {Int} height 
+ *
+ * @param {Int} width
+ * @param {Int} height
  * @param {Array<Int>} cells the array to use for the cells (default: new Uint8Array(width * height))
  */
-function Board(width=32, height=32, cells) {
+function Board(width = 32, height = 32, cells) {
   this.width = width
   this.height = height
   // We'll store our cells in a 1D typed array.
@@ -21,28 +21,29 @@ function Board(width=32, height=32, cells) {
 
 /**
  * indexFor(coords: [row: int, col: int]) -> int
- * 
+ *
  * Given an array of coordinates [row, col], return the index of that cell in this
  * board's cells array.
  */
-Board.prototype.indexFor = function([row, col]) {
+Board.prototype.indexFor = function ([row, col]) {
   // OMG, a destructured parameter! ⬆️⬆️⬆️⬆️⬆️⬆️
-  //
+  var row = row;
+  var col = col;
   // This digs into the array we were passed, plucks out the first
   // two elements, and names them row and col. Any other elements
   // are ignored.
   //
-  // http://2ality.com/2015/01/es6-destructuring.html  
-  
+  // http://2ality.com/2015/01/es6-destructuring.html
+
   // Return undefined if we're out of bounds
   if (row < 0 || row >= this.height || col < 0 || col >= this.width)
-    return  
+    return
   return row * this.width + col
 }
 
 /**
  * get(coords: [row: int, col: int]) -> uint8
- * 
+ *
  * Get the value of the board at coords.
  */
 Board.prototype.get = function (coords) {
@@ -51,51 +52,89 @@ Board.prototype.get = function (coords) {
 
 /**
  * set(coords: [row: int, col: int], value: uint8)
- * 
+ *
  * Set the value of the board at coords to value.
  */
-Board.prototype.set = function(coords, value) {
+Board.prototype.set = function (coords, value) {
   // TODO
+  this.cells[this.indexFor(coords)] = value;
 }
 
 /**
  * livingNeighbors(coords: [row: int, col: int])
- * 
+ *
  * Return the count of living neighbors around a given coordinate.
  */
-Board.prototype.livingNeighbors = function([row, col]) {
+Board.prototype.livingNeighbors = function ([row, col]) {
   // TODO: Return the count of living neighbors.
+  var count = 0;
+  if (this.cells[this.indexFor([row, col + 1])] === 1) {
+    count++;
+  }
+  if (this.cells[this.indexFor([row, col - 1])] === 1) {
+    count++;
+  }
+  if (this.cells[this.indexFor([row + 1, col])] === 1) {
+    count++;
+  }
+  if (this.cells[this.indexFor([row - 1, col])] === 1) {
+    count++;
+  }
+  if (this.cells[this.indexFor([row + 1, col + 1])] === 1) {
+    count++;
+  }
+  if (this.cells[this.indexFor([row - 1, col + 1])] === 1) {
+    count++;
+  }
+  if (this.cells[this.indexFor([row - 1, col - 1])] === 1) {
+    count++;
+  }
+  if (this.cells[this.indexFor([row + 1, col - 1])] === 1) {
+    count++;
+  }
+  return count;
 }
 
 /**
  * toggle(coords: [row: int, col: int])
- * 
+ *
  * Toggle the cell at coords from alive to dead or vice versa.
  */
-Board.prototype.toggle = function(coords) {
+Board.prototype.toggle = function (coords) {
   // TODO
+  if (this.cells[this.indexFor(coords)] === 1) {
+    this.cells[this.indexFor(coords)] = 0;
+  }
+  else {
+    this.cells[this.indexFor(coords)] = 1
+  }
 }
 
 /**
  * Give the vitals of a cell (its current state, and how many living neighbors it
- * currently has), return whether it will be alive in the next tick. 
- * 
- * @param {Boolean} isAlive 
- * @param {Number} numLivingNeighbors 
+ * currently has), return whether it will be alive in the next tick.
+ *
+ * @param {Boolean} isAlive
+ * @param {Number} numLivingNeighbors
  */
 function conway(isAlive, numLivingNeighbors) {
-  // TODO
+  if (numLivingNeighbors === 2 || numLivingNeighbors === 3) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 /**
  * Given a present board, a future board, and a rule set, apply
  * the rules to the present and modify the future.
- * 
- * @param {Board} present 
+ *
+ * @param {Board} present
  * @param {Board!} future (is mutated)
  * @param {(Boolean, Int) -> Boolean} rules (default: conway)
  */
-function tick(present, future, rules=conway) {
+function tick(present, future, rules = conway) {
   // TODO
   return [future, present]
 }
